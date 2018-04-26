db010318647613541a03a839eadf0f3ee85a9eee
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-04-26 12:43:20 -0700

    started jsonutils file (not finish)

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,45 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
+        /* Sandwich info. Each sandwich's info in an element of the "list" array */
+        final String SWM_LIST = "list";
+
+        /* Message code */
+        final String SWM_MESSAGE_CODE = "cod";
+
+        /* String array to hold each day's weather String */
+        String[] parseSandwichData = null;
+
+        JSONObject sandwichJSON = new JSONObject(json);
+
+        /* check for error object */
+        if(sandwichJSON.has(SWM_MESSAGE_CODE)){
+            int errorCode = sandwichJSON.getInt(SWM_MESSAGE_CODE);
+            switch (errorCode) {
+                case HttpURLConnection.HTTP_OK:
+                    break;
+                case HttpURLConnection.HTTP_NOT_FOUND:
+                    /* invalid location */
+                    return null;
+                default:
+                    /* server down */
+                    return null;
+            }
+        }
+
+        JSONArray sandwichArray = sandwichJSON.getJSONArray(SWM_LIST);
+
+        parseSandwichData = new String[sandwichArray.length()];
+
         return null;
     }
 }
