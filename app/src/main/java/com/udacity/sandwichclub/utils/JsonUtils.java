535a0ee66f94ea1192b5fedcb1ab0fde1d60fd8b
buntoro tandjaja <crowduckseq@buntoros-MacBook-Pro.local>
2018-04-27 14:14:42 -0700

    read json & return sandwich object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,46 +1,62 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) throws JSONException {
-        /* Sandwich info. Each sandwich's info in an element of the "list" array */
-        final String SWM_LIST = "list";
+    public static Sandwich parseSandwichJson(String json) {
+        /* get sandwich info from json constants */
+        final String GET_NAME = "name";
+        final String SW_NAME = "mainName";
+        final String SW_ORIGIN = "placeOfOrigin";
+        final String SW_DESCRIPTION = "description";
+        final String SW_IMG = "image";
+        final String SW_KNOWN_AS = "alsoKnownAs";
+        final String SW_INGREDIENT = "ingredients";
 
-        /* Message code */
-        final String SWM_MESSAGE_CODE = "cod";
+        /* declarations variables for a Sandwich */
+        String sandwichName, placeOfOrigin, description;
+        List<String> alsoKnownAs = new ArrayList<>(), ingredients = new ArrayList<>();
+        String imageLink;
 
-        /* String array to hold each day's weather String */
-        String[] parseSandwichData = null;
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            /* (error check 1) check error here? */
 
-        JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject name = sandwichJSON.getJSONObject(GET_NAME);
+            sandwichName = name.getString(SW_NAME);
+            JSONArray knownAsArray = name.getJSONArray(SW_KNOWN_AS);
+            if (knownAsArray.length() == 0)
+                alsoKnownAs = null;
+            else
+                alsoKnownAs.add(knownAsArray.getString(0));
 
-        /* check for error object */
-        if(sandwichJSON.has(SWM_MESSAGE_CODE)){
-            int errorCode = sandwichJSON.getInt(SWM_MESSAGE_CODE);
-            switch (errorCode) {
-                case HttpURLConnection.HTTP_OK:
-                    break;
-                case HttpURLConnection.HTTP_NOT_FOUND:
-                    /* invalid location */
-                    return null;
-                default:
-                    /* server down */
-                    return null;
+            placeOfOrigin = sandwichJSON.getString(SW_ORIGIN);
+            description = sandwichJSON.getString(SW_DESCRIPTION);
+            imageLink = sandwichJSON.getString(SW_IMG);
+            JSONArray ingredientsJSONArr = sandwichJSON.getJSONArray(SW_INGREDIENT);
+            for (int i = 0; i < ingredientsJSONArr.length(); i++) {
+                ingredients.add(ingredientsJSONArr.getString(i));
             }
+//            Log.v("TAG", sandwichName);
+//            Log.v("TAG", placeOfOrigin);
+//            Log.v("TAG", description);
+//            Log.v("TAG", imageLink);
+//            Log.v("TAG", ingredients.toString());
+//            Log.v("TAG", alsoKnownAs.toString());
+            return new Sandwich(sandwichName, alsoKnownAs, placeOfOrigin, description, imageLink, ingredients);
+        } catch (JSONException e) {
+            Log.v("JSON", "invalid JSON data");
+            return null;
         }
-
-        JSONArray sandwichArray = sandwichJSON.getJSONArray(SWM_LIST);
-
-        parseSandwichData = new String[sandwichArray.length()];
-
-        return null;
     }
 }
